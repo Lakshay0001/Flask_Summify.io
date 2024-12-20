2e7f7301ffa41ab41eb9b9215c2d7d39aa9270e1
Lakshay0001 <114854037+Lakshay0001@users.noreply.github.com>
2024-12-20 19:33:31 +0530

    Update summary.js

--- static/js/summary.js
+++ static/js/summary.js
@@ -1,23 +1,60 @@
 const speakButton = document.getElementById("speakButton");
-      const summaryText = document.getElementById("summaryText");
+const summaryText = document.getElementById("summaryText");
+const audioPlayer = document.createElement("audio");
 
-      speakButton.addEventListener("click", () => {
-        const text = summaryText.innerText;
+speakButton.addEventListener("click", async () => {
+  const text = summaryText.innerText;
 
-        fetch("/speak", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ text }),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            if (data.error) {
-              alert(data.error);
-            } else {
-              alert(data.message);
-            }
-          })
-          .catch((error) => console.error("Error:", error));
-      });
+  if (!text.trim()) {
+    alert("No text!");
+    return;
+  }
+  try {
+    const response = await fetch("/speak", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text }),
+    });
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+    const result = await response.json();
+    if (result.status === "success") {
+      audioPlayer.src = result.audio;
+      audioPlayer.play();
+    } else {
+      alert(`${result.message}`);
+    }
+  } catch (error) {
+    alert(`${error.message}`);
+  }
+});
+
+
+
+
+// const speakButton = document.getElementById("speakButton");
+// const summaryText = document.getElementById("summaryText");
+
+// speakButton.addEventListener("click", () => {
+//   const text = summaryText.innerText;
+
+//   fetch("/speak", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ text }),
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       if (data.error) {
+//         alert(data.error);
+//       } else {
+//         alert(data.message);
+//       }
+//     })
+//     .catch((error) => console.error("Error:", error));
+// });
